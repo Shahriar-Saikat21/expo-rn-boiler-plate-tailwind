1a56562e74e42c42d7a4e7a5d03468404184815b
saikat <saikatbass@gmail.com>
2025-06-28 14:47:43 +0600

    React Native with Tailwind Boilerplate

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className=" text-2xl text-red-600 font-bold">
+        Welcome to Expo React Native
+      </Text>
+    </View>
+  );
+}
